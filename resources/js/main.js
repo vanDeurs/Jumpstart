7bee9b1c39ef011ebc4a1b952ddfdadee832089f
Alexander Wilson van Deurs <alexwvd@outlook.com>
2017-06-28 21:37:30 +0200

    DONE.

--- resources/js/main.js
+++ resources/js/main.js
@@ -7,5 +7,18 @@ $(document).ready(function(){
     draggable: false,
     pauseOnFocus: false,
     pauseOnHover: false
+});
+
+  var lastScrollTop = 0;
+  $(window).scroll(function(){
+    var scrollTop = $(this).scrollTop();
+    if (scrollTop - lastScrollTop > 50){
+      var navHeight = $('.navbar').css('height');
+      $('.navbar').animate({top: '-' + navHeight}, 150);
+      lastScrollTop = scrollTop;
+    } else if (lastScrollTop - scrollTop > 50) {
+      $('.navbar').animate({top: '0px'}, 150);
+      lastScrollTop = scrollTop;
+    }
   });
 });
